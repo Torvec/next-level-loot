29f2505fca46361b8f2a03b418d24c98b80541b7
Edward Vonschondorf <edvonschondorf@gmail.com>
2024-11-09 22:35:32 -0800

    Overhauled footer

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,131 +1,118 @@
-import { Moon, Sun, Monitor, ExternalLink } from "lucide-react";
+import { Moon, Sun, Monitor, ExternalLink, Code, Gamepad2 } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
 export default function Footer() {
+  const ExtLink = ({ href, children }: { href: string; children: string }) => {
+    return (
+      <a
+        href={href}
+        target="_blank"
+        rel="noopener external"
+        className="flex w-max items-center gap-2 text-yellow-300 hover:text-yellow-400"
+      >
+        <span>{children}</span>
+        <ExternalLink size={16} />
+      </a>
+    );
+  };
+
   return (
     <footer className="border-t border-neutral-700 bg-neutral-900/25">
-      <div className="container mx-auto space-y-8 pb-32 pt-16">
+      <div className="container mx-auto space-y-16 pb-32 pt-16">
         <div className="flex items-start justify-between">
-          <h3 className="mb-4 text-xl font-bold uppercase">The Loot Vault</h3>
-          {/* Developer */}
-          <div>
-            <h3 className="mb-4 font-bold">Developer</h3>
-            <ul className="space-y-2">
-              <li className="max-w-[22ch] text-neutral-400">
-                Designed and developed by Edward Vonschondorf
+          {/* Logo */}
+          <h3 className="mb-4 flex w-1/4 items-center gap-2 text-xl font-bold uppercase">
+            <Gamepad2 size={24} />
+            The Loot Vault
+          </h3>
+
+          {/* Project */}
+          <div className="w-1/4">
+            <h3 className="mb-4 font-bold">Project</h3>
+            <ul className="space-y-2 text-neutral-400">
+              <li>Made with Next JS</li>
+              <li className="flex gap-2">
+                GitHub:
+                <ExtLink href="https://github.com/Torvec/loot_vault_next">
+                  Repository
+                </ExtLink>
+              </li>
+              <li className="text-pretty">
+                Based off of my first coding bootcamp group project:
               </li>
               <li>
-                <a
-                  href="https://edward-vonschondorf.dev"
-                  target="_blank"
-                  rel="noopener external"
-                  className="flex w-max items-center gap-2 text-orange-500 hover:text-orange-400"
-                >
-                  <span>Edward-vonschondorf.dev</span>
-                  <ExternalLink size={16} />
-                </a>
+                <ExtLink href="https://torvec.github.io/Loot-Vault/">
+                  Loot Vault v1 Site
+                </ExtLink>
               </li>
               <li>
-                <a
-                  href="https://github.com/Torvec"
-                  target="_blank"
-                  rel="noopener external"
-                  className="flex w-max items-center gap-2 text-orange-500 hover:text-orange-400"
-                >
-                  <span>My GitHub</span>
-                  <ExternalLink size={16} />
-                </a>
+                <ExtLink href="https://github.com/Torvec/Loot-Vault">
+                  Loot Vault v1 Repository
+                </ExtLink>
               </li>
             </ul>
           </div>
           {/* API's Used */}
-          <div>
+          <div className="w-1/4">
             <h3 className="mb-4 font-bold">API&apos;s</h3>
             <ul className="space-y-2 text-neutral-400">
               <li className="flex gap-2">
                 Best Deals:
-                <a
-                  href="https://apidocs.cheapshark.com/"
-                  target="_blank"
-                  rel="noopener external"
-                  className="flex w-max items-center gap-1 text-orange-500 hover:text-orange-400"
-                >
-                  <span>Cheapshark</span>
-                  <ExternalLink size={16} />
-                </a>
+                <ExtLink href="https://apidocs.cheapshark.com/">
+                  Cheapshark
+                </ExtLink>
               </li>
               <li className="flex gap-2">
-                Highest Rated/Search:
-                <a
-                  href="https://apidocs.cheapshark.com/"
-                  target="_blank"
-                  rel="noopener external"
-                  className="flex w-max items-center gap-1 text-orange-500 hover:text-orange-400"
-                >
-                  <span>RAWG</span>
-                  <ExternalLink size={16} />
-                </a>
+                Highest Rated:
+                <ExtLink href="https://api.rawg.io/docs/">RAWG</ExtLink>
+              </li>
+              <li className="flex gap-2">
+                Search:
+                <ExtLink href="https://api.rawg.io/docs/">RAWG</ExtLink>
               </li>
               <li className="flex gap-2">
                 Free Games:
-                <a
-                  href="https://www.gamerpower.com/api-read"
-                  target="_blank"
-                  rel="noopener external"
-                  className="flex w-max items-center gap-1 text-orange-500 hover:text-orange-400"
-                >
-                  <span>Gamerpower</span>
-                  <ExternalLink size={16} />
-                </a>
+                <ExtLink href="https://www.gamerpower.com/api-read">
+                  Gamerpower
+                </ExtLink>
               </li>
             </ul>
           </div>
-          {/* Projec */}
-          <div>
-            <h3 className="mb-4 font-bold">Project</h3>
-            <ul className="space-y-2 text-neutral-400">
-              <li>Made with Next JS</li>
+          {/* Developer */}
+          <div className="w-1/4">
+            <h3 className="mb-4 font-bold">Developer</h3>
+            <ul className="space-y-2">
+              <li className="text-neutral-400">
+                Designed and developed by Edward Vonschondorf
+              </li>
               <li>
-                Based off of a coding bootcamp group Project:
-                <ul className="flex gap-16 py-2">
-                  <li>
-                    <a
-                      href="https://torvec.github.io/Loot-Vault/"
-                      target="_blank"
-                      rel="noopener external"
-                      className="flex w-max items-center gap-1 text-orange-500 hover:text-orange-400"
-                    >
-                      <span>Loot Vault v1</span>
-                      <ExternalLink size={16} />
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="https://github.com/Torvec/Loot-Vault"
-                      target="_blank"
-                      rel="noopener external"
-                      className="flex w-max items-center gap-1 text-orange-500 hover:text-orange-400"
-                    >
-                      <span>GitHub</span>
-                      <ExternalLink size={16} />
-                    </a>
-                  </li>
-                </ul>
+                <ExtLink href="https://edward-vonschondorf.dev">
+                  Edward-vonschondorf.dev
+                </ExtLink>
+              </li>
+              <li>
+                <ExtLink href="https://github.com/Torvec">My Github</ExtLink>
               </li>
             </ul>
           </div>
         </div>
         {/* THEME TOGGLE */}
-        <div className="flex justify-end gap-1">
-          <Button className="rounded-xl bg-neutral-900">
-            <Moon size={24} />
-          </Button>
-          <Button className="rounded-xl bg-neutral-900">
-            <Sun size={24} />
-          </Button>
-          <Button className="rounded-xl bg-neutral-900">
-            <Monitor size={24} />
-          </Button>
+        <div className="flex justify-between">
+          <h4 className="flex gap-2 text-sm text-neutral-500">
+            <Code size={20} />
+            <span>2024</span>
+          </h4>
+          <div className="flex gap-1">
+            <Button className="rounded-xl bg-neutral-900">
+              <Moon size={24} />
+            </Button>
+            <Button className="rounded-xl bg-neutral-900">
+              <Sun size={24} />
+            </Button>
+            <Button className="rounded-xl bg-neutral-900">
+              <Monitor size={24} />
+            </Button>
+          </div>
         </div>
       </div>
     </footer>
