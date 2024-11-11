36d2a06c05ef41ad5c5d616b1a191ae9dd5c0fb6
Edward Vonschondorf <edvonschondorf@gmail.com>
2024-11-11 11:19:16 -0800

    Moved local font functions to fonts directory in styles and imported them into the main layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,12 @@
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { geistSans, geistMono } from "@/styles/fonts/local-font";
 import "@/styles/globals.css";
 import WishlistProvider from "@/lib/wishlist-provider";
 import Header from "@/components/ui/header";
 import Container from "@/components/ui/container";
 import Footer from "@/components/ui/footer";
-
-const geistSans = localFont({
-  src: "../styles/fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "../styles/fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "The Loot Vault",
