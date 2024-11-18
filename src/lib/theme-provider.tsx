84fda9f07e7ef7bd0981d5864f366b4794999cc8
Edward Vonschondorf <edvonschondorf@gmail.com>
2024-11-18 13:21:56 -0800

    Added ability to switch between light and dark themes as well as default to whatever the user prefers if neither are already set in local storage

---
+++ src/lib/theme-provider.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { createContext, useContext, useReducer, useEffect } from "react";
+
+type Themes = "" | "light" | "dark";
+
+const ThemeContext = createContext<Themes>("");
+const ThemeDispatchContext = createContext<
+  React.Dispatch<{ type: string; theme: Themes }>
+>(() => {});
+
+export default function ThemeProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [theme, dispatch] = useReducer(themeReducer, "");
+
+  useEffect(() => {
+    const storedTheme = initTheme("theme");
+    if (storedTheme !== "") {
+      dispatch({ type: "SET", theme: storedTheme as Themes });
+    } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      dispatch({ type: "SET", theme: "dark" });
+    } else {
+      dispatch({ type: "SET", theme: "light" });
+    }
+  }, []);
+
+  return (
+    <ThemeContext.Provider value={theme}>
+      <ThemeDispatchContext.Provider value={dispatch}>
+        {children}
+      </ThemeDispatchContext.Provider>
+    </ThemeContext.Provider>
+  );
+}
+
+function initTheme(key: string) {
+  const storedTheme = localStorage.getItem(key);
+  return storedTheme ? storedTheme : "";
+}
+
+export function useTheme() {
+  return useContext(ThemeContext);
+}
+
+export function useThemeDispatch() {
+  return useContext(ThemeDispatchContext);
+}
+
+function themeReducer(state: Themes, action: { type: string; theme: Themes }) {
+  switch (action.type) {
+    case "SET":
+      localStorage.setItem("theme", action.theme);
+      document.body.classList.remove("light", "dark");
+      document.body.classList.add(action.theme);
+      return action.theme;
+    default:
+      return state;
+  }
+}
