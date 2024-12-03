d36f08253f942d069048354565180f15acacf0fa
Edward Vonschondorf <edvonschondorf@gmail.com>
2024-12-02 21:09:42 -0800

    Complete redesign and refactor of header

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -2,27 +2,33 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { Gamepad2, Menu, Moon, Sun } from "lucide-react";
+import { Gamepad2, Menu, Moon, Sun, Scroll, Search } from "lucide-react";
 import { Button } from "./buttons/button";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
-import { useThemeDispatch } from "@/lib/theme-provider";
+import { useThemeDispatch, useTheme } from "@/lib/theme-provider";
 
 export default function Header() {
   return (
-    <header className="border-b bg-muted text-muted-foreground">
-      <div className="container mx-auto flex items-center justify-between gap-2 px-4 py-4 md:flex-row md:gap-0 xl:px-0">
-        <Logo />
-        <div className="hidden lg:block">
-          <NavBar />
+    <header className="border-b border-b-muted-foreground/25 bg-muted">
+      <div className="container mx-auto flex items-center justify-between px-4 py-4 xl:px-0">
+        <div className="flex gap-12">
+          <Logo />
+          <div className="hidden lg:block">
+            <NavBar />
+          </div>
         </div>
-        <div className="hidden lg:flex">
-          <ThemeSelect />
+        <div className="flex items-center gap-2 lg:gap-4">
+          <SearchBarButton />
+          <WishlistLinkIcon />
+          <DropDownMenu />
+          <div className="hidden lg:flex">
+            <ThemeSelect />
+          </div>
         </div>
-        <DropDownMenu />
       </div>
     </header>
   );
@@ -32,19 +38,21 @@ const Logo = () => (
   <Link
     href={"/"}
     prefetch={true}
-    className="flex items-center gap-2 text-lg font-bold uppercase text-foreground hover:text-muted-foreground"
+    className="flex items-center gap-2 text-lg font-black text-foreground hover:text-muted-foreground"
   >
-    <Gamepad2 size={24} />
+    <Gamepad2 size={22} />
     The Loot Vault
   </Link>
 );
 
 const NavBar = () => (
-  <nav className="flex flex-col font-medium lg:flex-row lg:items-center lg:justify-center">
+  <nav className="flex flex-col gap-4 text-sm uppercase lg:flex-row lg:items-center lg:justify-center">
     <NavLink href="/best-deals">Best Deals</NavLink>
     <NavLink href="/highest-rated">Highest Rated</NavLink>
     <NavLink href="/free-games">Free Games</NavLink>
-    <NavLink href="/wishlist">My Wishlist</NavLink>
+    <div className="lg:hidden">
+      <NavLink href="/wishlist">Wishlist</NavLink>
+    </div>
   </nav>
 );
 
@@ -54,33 +62,60 @@ const NavLink = ({ href, children }: { href: string; children: string }) => (
     prefetch={true}
     className={`${
       usePathname().startsWith(href)
-        ? "text-gold-foreground lg:border-b-2 lg:border-gold-foreground"
+        ? "text-gold-foreground lg:border-b lg:border-gold-foreground"
         : ""
-    } block w-full p-4 hover:bg-muted-foreground/75 hover:text-gold lg:inline lg:w-max lg:border-b-2 lg:py-2 lg:hover:border-b-2 lg:hover:border-muted-foreground lg:hover:bg-transparent lg:hover:text-foreground`}
+    } w-full px-2 hover:bg-muted-foreground/50 hover:text-gold lg:w-max lg:py-2 lg:hover:text-foreground`}
   >
     {children}
   </Link>
 );
 
+const SearchBarButton = () => {
+  return (
+    <Button className="rounded-full border border-muted-foreground/50 bg-background text-muted-foreground hover:border-foreground/50 hover:bg-background hover:text-foreground lg:space-x-24">
+      <span className="hidden lg:inline">Search</span>
+      <Search />
+    </Button>
+  );
+};
+
+const WishlistLinkIcon = () => {
+  return (
+    <Link
+      href="/wishlist"
+      className={`${usePathname().startsWith("/wishlist") ? "text-gold-foreground" : ""} hidden hover:text-muted-foreground lg:inline`}
+    >
+      <Scroll size={20} />
+    </Link>
+  );
+};
+
 const ThemeSelect = () => {
   const dispatch = useThemeDispatch();
+  const currentTheme = useTheme();
 
   return (
     <div className="divide-x">
-      <Button
-        onClick={() => dispatch({ type: "SET", theme: "dark" })}
-        className="rounded-l-xl rounded-r-none bg-muted-foreground"
-        aria-label="Switch to dark theme"
-      >
-        <Moon />
-      </Button>
-      <Button
-        onClick={() => dispatch({ type: "SET", theme: "light" })}
-        className="rounded-l-none rounded-r-xl bg-muted-foreground"
-        aria-label="Switch to light theme"
-      >
-        <Sun />
-      </Button>
+      {currentTheme === "light" && (
+        <Button
+          onClick={() => dispatch({ type: "SET", theme: "dark" })}
+          className="bg-muted-foreground/60 text-foreground hover:text-background"
+          aria-label="Switch to dark theme"
+          size="icon"
+        >
+          <Moon />
+        </Button>
+      )}
+      {currentTheme === "dark" && (
+        <Button
+          onClick={() => dispatch({ type: "SET", theme: "light" })}
+          className="bg-muted-foreground/60 text-foreground hover:text-background"
+          aria-label="Switch to light theme"
+          size="icon"
+        >
+          <Sun />
+        </Button>
+      )}
     </div>
   );
 };
@@ -88,7 +123,7 @@ const ThemeSelect = () => {
 const DropDownMenu = () => {
   return (
     <Popover>
-      <PopoverTrigger className="lg:hidden">
+      <PopoverTrigger className="text-muted-foreground lg:hidden">
         <Menu />
       </PopoverTrigger>
       <PopoverContent className="space-y-4 border-muted-foreground bg-secondary">
